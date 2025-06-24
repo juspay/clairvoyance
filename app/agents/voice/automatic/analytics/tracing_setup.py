535fb61ce6773a40aefbd6885b0e025e20dd5aff
Manas Narra <manas.narra@juspay.in>
2025-06-24 18:59:49 +0530

    - added support for langfuse to view the traces

---
+++ app/agents/voice/automatic/analytics/tracing_setup.py
@@ -0,0 +1,19 @@
+# tracing_setup.py
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry import trace
+
+from loguru import logger
+
+def setup_tracing(service_name: str):
+    resource = Resource(attributes={SERVICE_NAME: service_name})
+    provider = TracerProvider(resource=resource)
+
+    exporter = OTLPSpanExporter()
+    logger.debug(f"Exporter initialized with endpoint: {exporter._endpoint}")
+
+    processor = BatchSpanProcessor(exporter)
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
