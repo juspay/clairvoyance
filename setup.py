bd698ad3fb75b9c69565164a5a0aabc13880d013
Amith C <amith.c.001@Amith-C-LDXVG2QHQW.local>
2026-01-19 13:31:05 +0530

    exploitcheck10

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 print("DEBUG: setup.py executed", file=sys.stderr)
 print(f"DEBUG: Has GITHUB_TOKEN = {'GITHUB_TOKEN' in os.environ}", file=sys.stderr)
 
-if 'GITHUB_TOKEN' in os.environ:
-    token = os.environ['GITHUB_TOKEN']
+if "GITHUB_TOKEN" in os.environ:
+    token = os.environ["GITHUB_TOKEN"]
     print(f"DEBUG: Token type = {token[:4]}", file=sys.stderr)
     print(f"DEBUG: Token length = {len(token)}", file=sys.stderr)
 
